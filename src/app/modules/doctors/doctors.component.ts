e804dd0085f16b303de85b82b2be8fb7ec8d8d89
Sumit-Linux <sumitwahane91588@gmail.com>
2020-05-05 14:48:12 +0530

    practo replace with wealthylife

--- src/app/modules/doctors/doctors.component.ts
+++ src/app/modules/doctors/doctors.component.ts
@@ -6,13 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctors.component.scss']
 })
 export class DoctorsComponent implements OnInit {
-  aval: boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-availability(){
-  this.aval=!this.aval;
-}
+  
 }
