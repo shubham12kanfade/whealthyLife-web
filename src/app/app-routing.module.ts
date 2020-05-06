e0244dde74fc5f114c9d1e4e0893704f0e336c94
Sumit-Linux <sumitwahane91588@gmail.com>
2020-05-06 15:08:50 +0530

    doctor registration done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DoctorRegisterComponent } from './component/doctor-register/doctor-register.component';
 
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
+  {path:'doctor_signup',component:DoctorRegisterComponent},
   {path: '',loadChildren: () => import('./modules/home/home.module').then(h => h.HomeModule)},
  
   {path: 'accounts',loadChildren: () => import('./modules/user-accounts/user-accounts.module').then(us => us.UserAccountsModule)},
   {path: '',loadChildren: () => import('./modules/user-drive/user-drive.module').then(ud => ud.UserDriveModule )},
-
+  {path:'doctors',loadChildren:() => import('./modules/doctors/doctors.module').then(dd => dd.DoctorsModule)},
   {path: 'contact',loadChildren: () => import('./modules/contact-us/contact-us.module').then(c => c.ContactUsModule)},
   {path: 'about',loadChildren: () => import('./modules/about/about.module').then(a => a.AboutModule)},
   {path: 'bookappointment',loadChildren: () => import('./modules/bookappoiment/bookappoiment.module').then(b => b.BookappoimentModule)},
