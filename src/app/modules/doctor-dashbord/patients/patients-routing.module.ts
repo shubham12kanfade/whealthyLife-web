d1e5f46f159b1764b8f92ffd4a249a20517cf2d0
Maroti Kathoke <marotikathoke@gmail.com>
2020-05-13 23:13:02 +0530

    adding sumit branch patient

--- src/app/modules/doctor-dashbord/patients/patients-routing.module.ts
+++ src/app/modules/doctor-dashbord/patients/patients-routing.module.ts
@@ -1,12 +1,16 @@
+import { AddPatientComponent } from './add-patient/add-patient.component';
 import { PatientsComponent } from './patients.component';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { pathToFileURL } from 'url';
 
 
 const routes: Routes = [
   {
     path:'',component:PatientsComponent
+  },
+  {path:'add_patient',component:AddPatientComponent
   }
 ];
 
--- src/app/modules/doctor-dashbord/patients/patients.module.ts
+++ src/app/modules/doctor-dashbord/patients/patients.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PatientsRoutingModule } from './patients-routing.module';
+import { AddPatientComponent } from './add-patient/add-patient.component';
 
 
 @NgModule({
-  declarations: [PatientsComponent],
+  declarations: [PatientsComponent, AddPatientComponent],
   imports: [
     CommonModule,
     PatientsRoutingModule
+
   ]
 })
 export class PatientsModule { }
