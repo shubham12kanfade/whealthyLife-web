f6aa76696fdb2affc9d62a46cb4fddc29796e083
kamlesh21 <kamleshbahekar027@gmail.com>
2020-05-23 00:08:22 +0530

    material table

--- src/app/modules/doctor-dashbord/settings/practice-staff/add-practice-model/add-practice-model.component.ts
+++ src/app/modules/doctor-dashbord/settings/practice-staff/add-practice-model/add-practice-model.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  position: string;
+  NAME: string;
+  TYPE: string;
+  STREANGTH: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 'This doctor', NAME: '', TYPE: '', STREANGTH: '' },
+  { position: 'Daily Schedule', NAME: '', TYPE: '', STREANGTH: '' },
+  { position: 'Appointments Confirmation', NAME: '', TYPE: '', STREANGTH: '' },
+  { position: 'All doctors', NAME: '', TYPE: '', STREANGTH: '' },
+
+];
+
 
 @Component({
   selector: 'app-add-practice-model',
@@ -6,10 +23,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-practice-model.component.scss']
 })
 export class AddPracticeModelComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'NAME', 'TYPE', 'STREANGTH'];
+  dataSource: any;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA)
+    this.dataSource.paginator = this.paginator;
   }
+
 
 }
