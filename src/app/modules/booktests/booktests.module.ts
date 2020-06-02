15133a4f2fb99ea1bf1c479c3dba08dbe36d5cbb
kamlesh21 <kamleshbahekar027@gmail.com>
2020-06-02 17:15:12 +0530

    chat

--- src/app/modules/booktests/booktests.module.ts
+++ src/app/modules/booktests/booktests.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BooktestsRoutingModule } from './booktests-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SelectCityComponent } from './select-city/select-city.component';
--- src/app/modules/chat-with-doctor/chat-with-doctor.module.ts
+++ src/app/modules/chat-with-doctor/chat-with-doctor.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ChatWithDoctorRoutingModule } from './chat-with-doctor-routing.module';
 import { ChatNowComponent } from './chat-now/chat-now.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ChatWithDoctorComponent } from './chat-with-doctor.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 
 @NgModule({
   declarations: [ChatNowComponent,ChatWithDoctorComponent],
   imports: [
     CommonModule,
-    ChatWithDoctorRoutingModule
+    ChatWithDoctorRoutingModule,
+    SharedModule,
+    CarouselModule,
   ]
 })
 export class ChatWithDoctorModule { }
