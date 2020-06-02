29d8a87191af67d2fc4b9c3b1d377e1eeec6b5ce
Maroti Kathoke <marotikathoke@gmail.com>
2020-06-02 23:43:33 +0530

    health feed

--- src/app/modules/doctor-dashbord/health-feed/health-feed.component.ts
+++ src/app/modules/doctor-dashbord/health-feed/health-feed.component.ts
@@ -1,4 +1,7 @@
+import { StartFreeComponent } from './start-free/start-free.component';
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-health-feed',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthFeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+  customOptions: OwlOptions = {
+    loop:true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    autoplay: false,
+    navSpeed: 300,
+    navText: ["", ""],
 
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      },
+      1040: {
+        items: 1
+      }
+    },
+    nav: false
+  };
   ngOnInit(): void {
+  }
+
+
+
+
+  startfreemodalopen() {
+    const dialogRef = this.dialog.open(StartFreeComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      
+    });
   }
 
 }
