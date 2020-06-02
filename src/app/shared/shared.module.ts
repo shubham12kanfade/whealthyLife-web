30772086367b210d15df1bce2bdf7857f4cb6d10
sunit deshmukh <sumitwahane91588@gmail.com>
2020-06-02 11:38:27 -0700

    done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,21 +3,27 @@ import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDialogModule} from '@angular/material/dialog';
-
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatTabsModule,
     MatIconModule,
-    MatDialogModule
-
+    MatDialogModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatRadioModule
   ],
   exports: [
     MatTabsModule,
     MatIconModule,
     MatDialogModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatRadioModule
 
   ]
 })
