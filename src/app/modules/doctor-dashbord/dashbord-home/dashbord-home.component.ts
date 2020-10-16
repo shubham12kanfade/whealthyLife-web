4dce70fc071dad6907448d6a9b58c24c3e7d547b
Sunit Deshmukh <sunitdk11@gmail.com>
2020-10-16 19:13:05 +0530

    changes in Book appointment, about us, footer, bookhealth

--- src/app/modules/doctor-dashbord/dashbord-home/dashbord-home.component.ts
+++ src/app/modules/doctor-dashbord/dashbord-home/dashbord-home.component.ts
@@ -1,3 +1,5 @@
+import { UserService } from './../../../services/user.service';
+import { DashboardAnalyticsService } from './../../../services/dashboard-analytics.service';
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
@@ -21,12 +23,29 @@ export class DashbordHomeComponent implements OnInit {
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
   ];
+  dashData: any;
 
 
 
-  constructor() { }
+  constructor(public DAS:DashboardAnalyticsService, public UserService:UserService) { 
+
+    const id= this.UserService.getUserInfo()
+
+    this.DAS.getdashboard(id._id).then(restData => {
+   
+
+    this.dashData=restData.data;
+    console.log("DashbordHomeComponent -> constructor -> this.dashData", this.dashData)
+      
+    }).catch(error => {
+    console.log("DashbordHomeComponent -> constructor -> error", error)
+      
+    })
+  }
 
   ngOnInit(): void {
+
+
   }
 
 
