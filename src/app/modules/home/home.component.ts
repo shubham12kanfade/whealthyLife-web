c8e320dc6796d68c5c781269999600b0663d7649
sunitaws <sunitdk11@gmail.com>
2020-10-19 14:04:10 +0530

    HomeComponent

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -39,36 +39,27 @@ export class HomeComponent implements OnInit {
   constructor(public SpecialityService:SpecialityService,
     public MainService:MainService) {
       SpecialityService.getSpecialization().then((resData)=>{
-          console.log("HomeComponent -> constructor -> resData", resData)
           this.Specialit=resData.data
         }).catch((error)=>{
           console.log("HomeComponent -> constructor -> error", error)
         })
 
       MainService.getLabs().then((resData)=>{
-        console.log("HomeComponent -> resData+++++++++++++++++", resData)
           this.labs=resData.data
       }).catch((eror)=>{
         console.log("HomeComponent -> eror", eror)
       })
 
       this.countryList = csc.getAllCountries();
-      console.log("HomeComponent -> this.countryList", this.countryList)
 
       this.stateList = csc.getStatesOfCountry("101");
-        console.log("HomeComponent -> getStateList -> stateList", this.stateList)
 
         SpecialityService.getTestMaster().then(testdata => {
-        console.log(": -----------------------------------");
-        console.log("HomeComponent -> testdata", testdata);
-        console.log(": -----------------------------------");
 
        testdata.data.map(ele => {
 
          this.searchData = ele.fullName
-         console.log(": ---------------------------------------");
-         console.log("HomeComponent -> searchData", this.searchData);
-         console.log(": ---------------------------------------");
+     
         })
       }).catch(err => {
         console.log(": -------------------------");
@@ -187,23 +178,9 @@ export class HomeComponent implements OnInit {
 
 
 
-  ngOnInit(): void {
+  ngOnInit() {
 
-    // this.filteredOptions = this.myControl.valueChanges
-    // .pipe(
-    //   startWith(''),
-    //   map(value => typeof value === 'string' ? value : value.name),
-    //   map(name => name ? this._filter(name) : this.options.slice())
-    // );
+   
   }
-  // displayFn(user: User): string {
-  //   return user && user.name ? user.name : '';
-  // }
-
-  // private _filter(name: string): User[] {
-  //   const filterValue = name.toLowerCase();
-
-  //   return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
-  // }
-
+ 
 }
