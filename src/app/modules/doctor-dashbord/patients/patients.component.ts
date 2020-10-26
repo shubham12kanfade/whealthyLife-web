dea30814ba4b3a7fca1c387e30d701b66d8c54e4
Ajinkya-2607 <71754129+Ajinkya-2607@users.noreply.github.com>
2020-10-26 17:56:20 +0530

    Updated

--- src/app/modules/doctor-dashbord/patients/patients.component.ts
+++ src/app/modules/doctor-dashbord/patients/patients.component.ts
@@ -1,13 +1,43 @@
 import { PaitentsService } from './../../../services/paitents.service';
 import { UserService } from './../../../services/user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
+export interface PeriodicElement {
+  appointmenType: string;
+  date: string;
+  time: string;
+  firstName: string;
+  lastName: string;
+  status: string;
+ }
+
+
 
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
   styleUrls: ['./patients.component.scss']
 })
-export class PatientsComponent implements OnInit {
+export class PatientsComponent implements OnInit  {
+
+  displayedColumns: string[] = ['position', 'paitentName', 'appointmentType', 'date', 'status', 'time'];
+  userAppointment: any =[];
+  doctormm: any = [];
+
+  dataSource = new MatTableDataSource<PeriodicElement>(this.userAppointment);
+
+
+  @ViewChild(MatPaginator) set paginator(value: MatPaginator) {
+    this.dataSource.paginator = value;
+  };
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('table', { static: true }) table;
+
+
 
 
   show: boolean=false;
@@ -17,6 +47,7 @@ export class PatientsComponent implements OnInit {
   activeTab = 'today';
   DoctId: any;
   paitent: any;
+  spin: boolean = true;
 
   alllPaitent: any = [];
   weekPaitent: any = [];
@@ -25,14 +56,18 @@ export class PatientsComponent implements OnInit {
   weekpaitent: any;
   todaypaitent: any;
   Allpaitent: any;
-  constructor(public userService : UserService, public paitentService : PaitentsService) {
+  doctor: any = [];
+  paitentId: any =[];
+  appointment: any =[];
+  appoType: any;
+  date: any;
+  constructor(public userService : UserService, public paitentService : PaitentsService,
+    ) {
 
-         this.DoctId= this.userService.getUserInfo()
-
-
+         this.DoctId= this.userService.getUserInfo();
 
         this.paitentService.getAllPaitent(this.DoctId._id).then(resData=>{
-          console.log("PatientsComponent -> constructor -> resData", resData);
+
            this.Allpaitent = resData.data.AllPaitent.length;
            this.todaypaitent = resData.data.TodayPaitent.length;
            this.weekpaitent = resData.data.WeeKPaitent.length;
@@ -55,12 +90,78 @@ export class PatientsComponent implements OnInit {
         }).catch(error =>{
           console.log("PatientsComponent -> constructor -> error", error);
         })
+
+
+        this.getAppoint();
+
+
+
    }
 
 
+   getAppoint(){
 
+    this.paitentService.getAppointment(this.DoctId._id).then(AppointmentRes => {
+    console.log(": -----------------------------------------------------------------");
+    console.log("PatientsComponent -> getAppoint -> AppointmentRes", AppointmentRes);
+    console.log(": -----------------------------------------------------------------");
+    for(let i = 0; i< AppointmentRes.data.length; i++){
+
+      this.doctor = AppointmentRes.data[i] && AppointmentRes.data[i].doctor && AppointmentRes.data[i].doctor._id
+      this.paitent = AppointmentRes.data[i] && AppointmentRes.data[i].petient && AppointmentRes.data[i].petient._id
+
+
+      if(this.DoctId._id == this.doctor){
+        this.appointment[i] = AppointmentRes.data[i]
+        console.log(": ---------------------------------------------------------------------");
+        console.log("PatientsComponent -> getAppoint -> this.appointment[i]", this.appointment[i]);
+        console.log(": ---------------------------------------------------------------------");
+      }
+
+    }
+
+    }).catch(error => {
+    console.log("PatientsComponent -> getAppoint -> error", error);
+    })
+
+
+   }
+
+   onClick(id){
+    this.paitentService.getAppointment(this.DoctId._id).then(Appointment => {
+      console.log(": -----------------------------------------------------------------");
+      console.log("PatientsComponent -> getAppoint -> Appointment", Appointment);
+      console.log(": -----------------------------------------------------------------");
+      for(let a = 0; a< Appointment.data.length; a++){
+
+        this.paitent = Appointment.data[a] && Appointment.data[a].petient && Appointment.data[a].petient._id
+
+          if(this.paitent == id){
+            this.userAppointment[a] = Appointment.data[a]
+            console.log(": ---------------------------------------------------------------------");
+            console.log("PatientsComponent -> getAppoint -> this.userAppointment[a]", this.userAppointment[a]);
+            console.log(": ---------------------------------------------------------------------");
+
+            this.dataSource.data = this.userAppointment;
+
+          }
+      }
+
+      }).catch(error => {
+      console.log("PatientsComponent -> getAppoint -> error", error);
+      })
+
+
+
+   }
 
   ngOnInit(): void {
+
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.spin = false;
+
   }
 
 
@@ -84,6 +185,18 @@ export class PatientsComponent implements OnInit {
     this.show=!this.show;
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
 
 }
+
+
+
+
