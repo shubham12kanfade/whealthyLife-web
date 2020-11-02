65481f64bdf2b0200e35ad34582bf2f0086183e5
Ajinkya-2607 <71754129+Ajinkya-2607@users.noreply.github.com>
2020-11-02 11:42:24 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   // apiUrl:"http://3.6.80.178:3311/api/v1/",
- apiUrl:"http://192.168.0.156:3311/api/v1/"
+ apiUrl:"http://192.168.0.178:3311/api/v1/"
   // apiUrl:"https://api.whealthylife.in/api/v1/",
 
 //  apiUrl:"http://localhost:3311/api/v1/"
