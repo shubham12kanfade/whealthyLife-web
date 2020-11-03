77ca243572d474214db77b32335ffad8dfc3ac14
sunitaws <sunitdk11@gmail.com>
2020-11-03 18:49:54 +0530

    Add chenges

--- src/app/modules/doctors/doctor-profile/doctor-profile.component.ts
+++ src/app/modules/doctors/doctor-profile/doctor-profile.component.ts
@@ -4,9 +4,10 @@ import { DoctorProfileService } from "./../../../services/doctor-profile.service
 import { Component, OnInit } from "@angular/core";
 import { MainService } from "src/app/services/main.service";
 import { ActivatedRoute } from "@angular/router";
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FeedBackPopComponent } from '../feed-back-pop/feed-back-pop.component';
 import csc from 'country-state-city';
+import * as moment from 'moment';
 
 @Component({
   selector: "app-doctor-profile",
@@ -25,79 +26,140 @@ export class DoctorProfileComponent implements OnInit {
   Awards: any;
   memberShip: any;
   currentDate: Date;
- 
+  date: Date;
+  slot2: any;
+  slot3: any;
+  slot4: any;
+  slot5: any;
+  Qa: any;
+  review: any;
+  Star: any;
+
   constructor(
     public mainService: MainService,
     public activatedRoutes: ActivatedRoute,
     public DoctorProfile: DoctorProfileService,
     public LocationS: LocationService,
-    public BookingPageService:BookingPageService,public dialog: MatDialog
-  ) { 
+    public BookingPageService: BookingPageService, public dialog: MatDialog
+  ) {
 
     this.currentDate = new Date();
-    console.log("DoctorProfileComponent -> this.currentDate", this.currentDate)
   }
   openDialog(): void {
     const dialogRef = this.dialog.open(FeedBackPopComponent, {
       width: '250px',
     });
   }
-getCitys(id){
-   const data=csc.getCityById(id)
-   return data.name
-}
-getStates(id){
-  const data=csc.getStateById(id);
- return data.name
-}
-getCountrys(id){
- const data=csc.getCountryById(id);
- return data.name
-}
+  getCitys(id) {
+    const data = csc.getCityById(id)
+    return data.name
+  }
+  getStates(id) {
+    const data = csc.getStateById(id);
+    return data.name
+  }
+  getCountrys(id) {
+    const data = csc.getCountryById(id);
+    return data.name
+  }
   ngOnInit(): void {
+
+
+
+
+
     this.activatedRoutes.params.subscribe((params) => {
       this.id = params["id"];
       this.getProfileDetails();
     });
     this.getaword()
     this.getDocMember()
-this.BookingPageService.getDegree(this.id).then((resData)=>{
-this.degree=resData.data
+    this.BookingPageService.getDegree(this.id).then((resData) => {
+      this.degree = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+    })
+
+
+    this.BookingPageService.getDoctorSlotId(this.id, { date: this.currentDate }).then((resData) => {
+      this.slot = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+    })
+
+    const data1 = moment().add(1, 'days').format('L');
+    this.BookingPageService.getDoctorSlotId(this.id, { date: data1 }).then((resData) => {
+      this.slot2 = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+    })
+
+    const data2 = moment().add(2, 'days').format('L');
+    this.BookingPageService.getDoctorSlotId(this.id, { date: data2 }).then((resData) => {
+      this.slot3 = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+    })
+
+
+
+    const data3 = moment().add(3, 'days').format('L');
+    this.BookingPageService.getDoctorSlotId(this.id, { date: data3 }).then((resData) => {
+      this.slot4 = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+    })
+
+
+    const data4 = moment().add(4, 'days').format('L');
+    this.BookingPageService.getDoctorSlotId(this.id, { date: data4 }).then((resData) => {
+      this.slot5 = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+    })
+
+
+    this.BookingPageService.getQa(this.id).then((resData) => {
+this.Qa=resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+
+    })
+this.BookingPageService.getReview(this.id).then((resData)=>{
+
+this.review=resData.data
 }).catch((err)=>{
-console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+console.log("err", err)
+  
 })
-
-
-this.BookingPageService.getDoctorSlotId(this.id,{date: this.currentDate}).then((resData)=>{
-  this.slot=resData.data
-  console.log("DoctorProfileComponent -> ngOnInit -> this.slot", this.slot)
+this.BookingPageService.getReview(this.id).then((resData)=>{
+console.log("resData", resData)
+this.Star=resData.data
 }).catch((err)=>{
-console.log("DoctorProfileComponent -> ngOnInit -> err", err)
+console.log("err", err)
+  
 })
   }
-  getDocMember(){
-    this.BookingPageService.getDroctorMember(this.id).then((resData)=>{
-      console.log("DoctorProfileComponent -> getDocMember -> resData", resData)
-this.memberShip=resData.data
-    }).catch((err)=>{
-    console.log("DoctorProfileComponent -> getDocMember -> err", err)
+  getDocMember() {
+    this.BookingPageService.getDroctorMember(this.id).then((resData) => {
+      this.memberShip = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> getDocMember -> err", err)
     })
   }
-  getaword(){
-    this.BookingPageService.getDroctorAward(this.id).then((resData)=>{
-    this.Awards=resData.data
-    console.log("DoctorProfileComponent -> getaword -> Awards", this.Awards)
-    }).catch((err)=>{
-    console.log("DoctorProfileComponent -> getaword -> err", err)
+  getaword() {
+    this.BookingPageService.getDroctorAward(this.id).then((resData) => {
+      this.Awards = resData.data
+    }).catch((err) => {
+      console.log("DoctorProfileComponent -> getaword -> err", err)
     })
   }
   getProfileDetails() {
-const data={ findId:this.id}
+    const data = { findId: this.id }
 
     this.DoctorProfile.getDoctorProfile(data)
       .then((resData) => {
         this.profileData = resData.data;
-        console.log("DoctorProfileComponent -> getProfileDetails -> profileData", this.profileData)
       })
       .catch((error) => {
         console.log(
@@ -109,4 +171,12 @@ const data={ findId:this.id}
   activefn(val) {
     this.active = val;
   }
+
+
+  TimeSlot(val) {
+    this.date = new Date();
+    this.date.setDate(this.date.getDate() + val);
+    return this.date
+  }
+
 }
