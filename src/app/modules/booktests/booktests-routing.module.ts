cc5077f025d041ddb4dcc93ce049e17135dc04e4
Ajinkya-2607 <71754129+Ajinkya-2607@users.noreply.github.com>
2020-11-18 15:56:42 +0530

    no change

--- src/app/modules/booktests/booktests-routing.module.ts
+++ src/app/modules/booktests/booktests-routing.module.ts
@@ -1,3 +1,4 @@
+// import { PopHealthComponent } from './pop-health/pop-health.component';
 import { BooktestsComponent } from './booktests.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,7 +8,8 @@ import { SelectCityComponent  }  from './select-city/select-city.component'
 const routes: Routes = [
 
   { path :'',component:BooktestsComponent},
-  { path :'select-city',component:SelectCityComponent}
+  { path :'select-city',component:SelectCityComponent},
+  // { path : 'pop-health',component: PopHealthComponent}
 ];
 
 @NgModule({
--- src/app/modules/booktests/booktests.module.ts
+++ src/app/modules/booktests/booktests.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { BooktestsRoutingModule } from './booktests-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SelectCityComponent } from './select-city/select-city.component';
+// import { PopHealthComponent } from './pop-health/pop-health.component';
+// import { ISOPopComponent } from './pop-health/iso-pop/iso-pop.component';
+// import { NABLPopComponent } from './pop-health/nabl-pop/nabl-pop.component';
+
 
 
 @NgModule({
-  declarations: [SelectCityComponent],
+  declarations: [SelectCityComponent,
+    // PopHealthComponent, ISOPopComponent, NABLPopComponent
+   ],
   imports: [
     CommonModule,
     BooktestsRoutingModule,
