d50537a85f2418963ebd8b58e9280c6a52adb068
SunitDeshmukh <sunitdk11@gmail.com>
2020-12-03 15:17:21 +0530

    changes in test

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class BookingService {
+export class BookingService { 
 
   constructor(private ApiCallService:ApiCallService) { }
 
